53b34ff7e284d5a17bbb5763fddc2ceb14712bf1
bg.chun <bg.chun@samsung.com>
2018-05-09 13:53:11 +0900

    update install script
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+USR_LIB_DIR=/usr/local/lib
+USR_INC_DIR=/usr/local/include
+KEYWORD_EZMQ_C=protocol-ezmq-c
+
+# COPY shared library to /usr/local/lib
 sudo cp out/linux/x86_64/release/libcezmq.so /usr/local/lib
-sudo cp -r  include /usr/local/include/ezmq-c
+
+# COPY header file to /usr/local/include/protocol-ezmq-c
+sudo rm -rf $USR_INC_DIR/$KEYWORD_EZMQ_C
+sudo mkdir $USR_INC_DIR/$KEYWORD_EZMQ_C
+sudo cp -rf ./include/* $USR_INC_DIR/$KEYWORD_EZMQ_C/
